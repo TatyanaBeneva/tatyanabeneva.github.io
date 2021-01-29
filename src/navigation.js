387ef72779c16d082d9a2586380f5cab79301ccb
TatyanaBeneva <64098144+TatyanaBeneva@users.noreply.github.com>
2021-01-29 15:57:46 +0200

    nav

--- src/navigation.js
+++ src/navigation.js
@@ -2,14 +2,26 @@ import React from 'react';
 import {
   Switch,
   Route
+<<<<<<< Updated upstream
 } from 'react-router-dom';
 import IntroPage from './pages/intro-page';
+=======
+} from 'react-router-dom'
+import IntroPage from './pages/intro-page'
+import AboutPage from './pages/about-page'
+import ContactPage from './pages/contact-page'
+>>>>>>> Stashed changes
 
 const Navigation = () => {
 
   return (
     <Switch>
       <Route path="/" exact component={IntroPage} />
+<<<<<<< Updated upstream
+=======
+      <Route path="/about" component={AboutPage} />
+      <Route path="/contact" component={ContactPage} />
+>>>>>>> Stashed changes
     </Switch>
   );
 };
