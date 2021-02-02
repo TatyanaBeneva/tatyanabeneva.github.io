ff10bdb937bf29456f0a7ce7dfb4618ae8623b6b
TatyanaBeneva <64098144+TatyanaBeneva@users.noreply.github.com>
2021-02-02 12:16:30 +0200

    Integrating server for sending emails from data in contact form

---
+++ server/index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const pass = require('./.vscode/settings.json')
+
+const app = express();
+const route = express.Router();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', route)
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// create a GET route
+app.get('/', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
+
+// create reusable transporter object using the default SMTP transport
+
+const transporter = nodemailer.createTransport({
+  port: 465,               // true for 465, false for other ports
+  host: "smtp.gmail.com",
+  auth: {
+    user: 'portfolio.t.v.b@gmail.com',
+    pass: pass['terminal.integrated.env.osx'].EMAIL_PASS,
+  },
+  secure: true,
+});
+
+route.post('/contact', (req, res) => {
+  const {name, email, message} = req.body
+
+  const mailToMe = {
+    from: 'portfolio.t.v.b@gmail.com',  // sender address
+    to: 'tatyana.v.beneva@gmail.com',   // list of receivers
+    subject: name,
+    text: message,
+    html: message + `<br> From: ${email}`,
+  };
+
+  transporter.sendMail(mailToMe, (error, info) => {
+    if(error) {
+      return console.log(error)
+    }
+
+    res.redirect('/contact')
+    // res.status(200).send({message: "Mail send", message_id: info.messageId})
+  });
+
+  const mailToClient = {
+    from: 'portfolio.t.v.b@gmail.com',  // sender address
+    to: email,   // list of receivers
+    subject: "Contact form send successfully!",
+    text: "Successful",
+    html: `Hi! Thank you ${name} for reaching out to me. I will try to answer you as soon as possible!<br> Greatings, T.B`
+  };
+
+
+  transporter.sendMail(mailToClient, (error, info) => {
+    if(error) {
+      return console.log(error)
+    }
+
+    res.redirect('/contact')
+    // res.status(200).send({message: "Mail send", message_id: info.messageId})
+  });
+})
+
--- src/navigation.js
+++ src/navigation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {
   Switch,
   Route
@@ -8,16 +8,40 @@ import AboutPage from './pages/about-page'
 import ContactPage from './pages/contact-page'
 import MyWorkPage from './pages/my-work-page'
 
-const Navigation = () => {
+class Navigation extends Component {
 
-  return (
-    <Switch>
-      <Route path="/" exact component={IntroPage} />
-      <Route path="/about" component={AboutPage} />
-      <Route path="/contact" component={ContactPage} />
-      <Route path="/work" component={MyWorkPage} />
-    </Switch>
-  );
+  state = {
+    data: null
+  };
+
+  componentDidMount() {
+      // Call our fetch function below once the component mounts
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('*');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body;
+  };
+
+  render() {
+    return (
+      <Switch>
+        <Route path="/" exact component={IntroPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/contact" component={ContactPage} />
+        <Route path="/work" component={MyWorkPage} />
+        <p>{this.state.data}</p>
+      </Switch>
+    );
+  }
 };
 
 export default Navigation
