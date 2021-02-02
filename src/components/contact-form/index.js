a0695fb4e9ab1f80dc4a8df7276e8bc863132ba6
TatyanaBeneva <64098144+TatyanaBeneva@users.noreply.github.com>
2021-02-02 14:38:19 +0200

    Make error handler for contact form

--- src/components/contact-form/index.js
+++ src/components/contact-form/index.js
@@ -7,9 +7,51 @@ const ContactForm = () => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [message, setMessage] = useState('')
+    const [errorName, setErrorName] = useState('')
+    const [errorEmail, setErrorEmail] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const handelValidation = () => {
+        // Name
+        if(!name){
+            setErrorName("Name field cannot be empty!")
+            return false
+        }
+
+        if(!name.match(/[A-Z][a-z]+ [A-Z][a-z]+/)){
+            setErrorName("Тhe first and second names must begin with a capital letter and continue with lowercase letters!")
+            return false
+        }
+
+        // Email
+        if(email === ''){
+            setErrorEmail("Email field cannot be empty!")
+            return false
+        }
+
+        if(!email.match(/[A-Za-z0-9.]+@[a-z]+.[a-z]+.?[a-z]+/)){
+            setErrorEmail("Email must be valid!")
+            return false
+        }
+
+        // Message
+        if(message === ''){
+            setErrorEmail("Message field cannot be empty!")
+            return false
+        }
+
+        return true
+    }
+
+    const contactSubmit = (e) => {
+        e.preventDefault()
+        if(!handelValidation()){
+            return
+        } 
+    }
 
     return (
-        <Form method="POST" action="/contact">
+        <Form onSubmit={contactSubmit}>
             <Input
                 onChange={e => setName(e.target.value)}
                 label={"What is your name?"}
