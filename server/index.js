6c71762fa3f8eb91a2542c251c137de04fcff04a
TatyanaBeneva <tatyana.v.beneva@gmail.com>
2021-02-05 15:39:44 +0200

    Build app changes

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path')
 const nodemailer = require('nodemailer');
 const cors = require('cors')
 const pass = require('./.vscode/settings.json')
@@ -12,6 +13,8 @@ const corsOptions = {
 const app = express();
 const route = express.Router();
 const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, '../public')))
 app.use(cors(corsOptions))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,7 +25,7 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
 app.get('/', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 // create reusable transporter object using the default SMTP transport
@@ -43,6 +46,7 @@ route.get('/contact', (req,res) => {
 
 route.post('/contact', (req, res) => {
   const {name, email, message} = req.body
+  console.log(req.body)
   const mailToMe = {
     from: 'portfolio.t.v.b@gmail.com',  // sender address
     to: 'tatyana.v.beneva@gmail.com',   // list of receivers
@@ -57,7 +61,7 @@ route.post('/contact', (req, res) => {
     }
 
     res.redirect('/contact')
-    // res.status(200).send({message: "Mail send", message_id: info.messageId})
+    res.status(200)
   });
 
   const mailToClient = {
@@ -75,7 +79,8 @@ route.post('/contact', (req, res) => {
     }
 
     res.redirect('/contact')
-    // res.status(200).send({message: "Mail send", message_id: info.messageId})
+    res.status(200)
+    // .send({message: "Mail send", message_id: info.messageId})
   });
 })
 
--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Navigation from './navigation'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Navigation />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
