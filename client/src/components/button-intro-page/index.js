1a7052fde00661424d94d8a9506c35f562992305
TatyanaBeneva <tatyana.v.beneva@gmail.com>
2021-02-07 12:46:51 +0200

    Button component in intro page made responsive and make button pink and larger when hover

--- client/src/components/button-intro-page/index.js
+++ client/src/components/button-intro-page/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import styled from 'styled-components'
-import Delayed from '../../utils/delayed-component'
 import { useHistory } from 'react-router-dom'
+import styled from 'styled-components'
+import { useMediaQuery } from 'react-responsive'
+import Delayed from '../../utils/delayed-component'
 import '../../fontawesome/fontawesome-free-5.15.2-web/css/all.min.css'
 
 const Button = () => {
+
+    const isLaptopOrTablet = useMediaQuery({ minWidth: 600, maxWidth: 1600 })
+    const isTablet = useMediaQuery({ minWidth: 481, maxWidth: 599 })
+    const isPhone = useMediaQuery({ maxWidth: 480 })
+
     const history = useHistory();
 
     const getBroken = (e) => {
@@ -14,22 +20,64 @@ const Button = () => {
     }
 
     return (
-        <Div>
-            <Delayed waitBeforeShow={1500}>
-                <DivText>
-                    <SpanText>
-                        Learn more about me and what I do
+        <div>
+            {isLaptopOrTablet &&
+                <Div600>
+                    <Delayed waitBeforeShow={1500}>
+                        <DivText>
+                            <SpanText>
+                                Learn more about me and what I do
                     </SpanText>
-                </DivText>
-                <ButtonTag onClick={getBroken}>
-                    <Span>
-                        <i className="fas fa-angle-down"></i>
-                    </Span>
-                </ButtonTag>
-            </Delayed>
-        </Div>
+                        </DivText>
+                        <ButtonTag onClick={getBroken}>
+                            <Span>
+                                <i className="fas fa-angle-down"></i>
+                            </Span>
+                        </ButtonTag>
+                    </Delayed>
+                </Div600>
+            }
+            {isTablet &&
+                <Div481>
+                    <Delayed waitBeforeShow={1500}>
+                        <DivText>
+                            <SpanText>
+                                Learn more about me and what I do
+                    </SpanText>
+                        </DivText>
+                        <ButtonTag onClick={getBroken}>
+                            <Span>
+                                <i className="fas fa-angle-down"></i>
+                            </Span>
+                        </ButtonTag>
+                    </Delayed>
+                </Div481>
+            }
+            {isPhone &&
+                <Div481>
+                    <Delayed waitBeforeShow={1500}>
+                        <DivText>
+                            <SpanText>
+                                Learn more about me and what I do
+                    </SpanText>
+                        </DivText>
+                        <ButtonTag onClick={getBroken}>
+                            <Span>
+                                <i className="fas fa-angle-down"></i>
+                            </Span>
+                        </ButtonTag>
+                    </Delayed>
+                </Div481>
+            }
+        </div>
     )
 }
+
+const Div481 = styled.div`
+    width: 100%;
+    text-align: center;
+    margin-top: 50px;
+`
 
 const SpanText = styled.span`
     background-image: linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0));
@@ -44,9 +92,14 @@ const DivText = styled.div`
 const Span = styled.span`
     font-size: 50px;
     color: white;
+
+    &:hover{
+        font-size: 60px;
+        color: pink;
+    }
 `
 
-const Div = styled.div`
+const Div600 = styled.div`
     width: 100%;
     position: fixed;
     text-align: center;
