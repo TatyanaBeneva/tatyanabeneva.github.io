921027cda569099a0de89781e19c3a1d6a4e96c4
TatyanaBeneva <tatyana.v.beneva@gmail.com>
2021-02-17 17:41:01 +0200

    My work page made responsive

--- client/src/components/project/index.js
+++ client/src/components/project/index.js
@@ -1,45 +1,96 @@
 import React from 'react'
 import styled from 'styled-components'
-import Skill from '../skill'
+import { useMediaQuery } from 'react-responsive'
 
 const Porject = ({ name, description, href, image, technologies }) => {
+
+    const isLaptop = useMediaQuery({ minWidth: 768 })
+    const isTablet = useMediaQuery({ maxWidth: 767 })
 
     const renderTechnologies = (() => {
         return technologies.map((tech, index) => {
             return (
-                <LiTech key={index}>
-                    {tech}
-                </LiTech>
+                <span>
+                    {isLaptop &&
+                        <LiTechLaptop key={index}>
+                            {tech}
+                        </LiTechLaptop>
+                    }
+                    {isTablet &&
+                        <LiTechTablet key={index}>
+                            {tech}
+                        </LiTechTablet>
+                    }
+                </span>
             )
         })
     })
 
     return (
-        <Li>
-            <DivInfo>
-                <Details>
-                    Project details
-                </Details>
-                <Name>{name}</Name>
-                <Description>{description}</Description>
-                <Details>
-                    Used Technologies:
-                </Details>
-                <ul style={{ padding: 0 }}>
-                    {renderTechnologies()}
-                </ul>
-                <a href={href}>
-                    <Button>View Live Project</Button>
-                </a>
-            </DivInfo>
-            <DivImage>
-                <Image src={image} />
-            </DivImage>
-        </Li>
+        <div>
+            {isLaptop &&
+                <LiLaptop>
+                    <DivInfo>
+                        <Details>
+                            Project details
+                        </Details>
+                        <Name>{name}</Name>
+                        <Description>{description}</Description>
+                        <Details>
+                            Used Technologies:
+                        </Details>
+                        <ul style={{ padding: 0 }}>
+                            {renderTechnologies()}
+                        </ul>
+                        <a href={href}>
+                            <Button>View Live Project</Button>
+                        </a>
+                    </DivInfo>
+                    <div>
+                        <Image src={image} />
+                    </div>
+                </LiLaptop>
+            }
+            {isTablet &&
+                <LiTablet>
+                    <DivInfo>
+                        <DetailsTablet>
+                            Project details
+                        </DetailsTablet>
+                        <NameTablet>{name}</NameTablet>
+                        <DescriptionTablet>{description}</DescriptionTablet>
+                        <DetailsTablet>
+                            Used Technologies:
+                        </DetailsTablet>
+                        <ul style={{ padding: 0 }}>
+                            {renderTechnologies()}
+                        </ul>
+                    </DivInfo>
+                    <div>
+                        <Image src={image} style={{ marginBottom: '30px' }} />
+                        <a href={href}>
+                            <ButtonTablet>View Live Project</ButtonTablet>
+                        </a>
+                    </div>
+                </LiTablet>
+            }
+        </div>
     )
 }
 
-const LiTech = styled.li`
+const LiTechTablet = styled.li`
+    display: inline-block;
+    margin-right: 5%;
+    margin-bottom: 5%;
+    width: auto;
+    border-radius: 20px;
+    background-color: white;
+    padding: 2%;
+    border: 2px solid #00b6ff;
+    font-size: 2.5vw;
+`
+
+const LiTechLaptop = styled.li`
     display: inline-block;
     margin-right: 5%;
     margin-bottom: 5%;
@@ -53,6 +104,25 @@ const LiTech = styled.li`
 
 const Image = styled.img`
     width: 100%;
+`
+const ButtonTablet = styled.button`
+    border: 2px solid white;
+    font-size: 2.5vw;
+    font-family: Jack, serif;
+    padding: 10px 20px;
+    border-radius: 25px;
+    background-color: transparent;
+    color: white;
+
+    &:focus {
+        outline: none;
+    }
+
+    &:hover {
+        background-color: #41f2b6;
+        color: black;
+        cursor: pointer;
+    }
 `
 
 const Button = styled.button`
@@ -74,12 +144,22 @@ const Button = styled.button`
         cursor: pointer;
     }
 `
-
+const DescriptionTablet = styled.p`
+    color: white;
+    font-size: 2.5vw;
+    letter-spacing: 2px;
+    margin-bottom: 40px;
+`
 const Description = styled.p`
     color: white;
     font-size: 1.5vw;
     letter-spacing: 2px;
     margin-bottom: 40px;
+`
+const NameTablet = styled.p`
+    color: white;
+    font-size: 5vw;
+    margin: 0 30px 0 0;
 `
 
 const Name = styled.p`
@@ -87,7 +167,11 @@ const Name = styled.p`
     font-size: 3vw;
     margin: 0 30px 0 0;
 `
-
+const DetailsTablet = styled.p`
+    color: #00b6ff;
+    font-size: 2.2vw;
+    letter-spacing: 3px;
+`
 const Details = styled.p`
     color: #00b6ff;
     font-size: 1.3vw;
@@ -97,11 +181,13 @@ const Details = styled.p`
 const DivInfo = styled.div`
     font-family: Jack, serif;
 `
-const DivImage = styled.div`
-
+const LiTablet = styled.li`
+    display: block;
+    width: 100%;
+    margin-bottom: 50px;
 `
 
-const Li = styled.li`
+const LiLaptop = styled.li`
     display: grid;
     grid-template-columns: 50% 50%;
     width: 100%;
--- client/src/components/work-projects/index.js
+++ client/src/components/work-projects/index.js
@@ -1,43 +1,81 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useMediaQuery } from 'react-responsive'
 import Project from '../project'
 import tutorials from './tutorials.png'
 import shoe from './shoe.png'
 import movies from './movies.png'
 
 const WorkProjects = () => {
+
+    const isLaptop = useMediaQuery({ minWidth: 768 })
+    const isTablet = useMediaQuery({ maxWidth: 767 })
+
     const academy = ['Html', 'CSS', 'JavaScript', 'Handlebars', 'Express', 'node.js', 'MongoDB']
     const shoes = ['Html', 'CSS', 'JavaScript', 'Handlebars', 'Firebase']
+
     return (
         <div>
-            <Ul>
-                <Project
-                name="Cours Academy - Back-end"
-                description="This is web site where you can have acount, to enroll in a programming course of your choice, to create, modify and delete courses that you create."
-                href="https://github.com/TatyanaBeneva/SoftUni-Courses/tree/master/Final%20Exam%20Preparation/JS-Back-End/Exam-From-1-November"
-                image={tutorials}
-                technologies={academy}
-                />
-                <Project
-                name="Shoes shop - Front-end with templates"
-                description="This is web site where you can have acount, by diferent shoes, create, modify and delete shoes offer that you create."
-                href="https://github.com/TatyanaBeneva/SoftUni-Courses/tree/master/Final%20Exam%20Preparation/JS-Applications/Exam-Form-18-August-2020/ShoeShelf"
-                image={shoe}
-                technologies={shoes}
-                />
-                <Project
-                name="Movies - Front-end with templates"
-                description="This is web site where you can have acount, to create movies reviews, see other member reviews, modify and delete movies reviews that you create and like others."
-                href="https://github.com/TatyanaBeneva/SoftUni-Courses/tree/master/Final%20Exam%20Preparation/JS-Applications/Exam-From-08-August-2020/Movies"
-                image={movies}
-                technologies={shoes}
-                />
-            </Ul>
+            {isLaptop &&
+                <UlLaptop>
+                    <Project
+                        name="Cours Academy - Back-end"
+                        description="This is web site where you can have acount, to enroll in a programming course of your choice, to create, modify and delete courses that you create."
+                        href="https://github.com/TatyanaBeneva/SoftUni-Courses/tree/master/Final%20Exam%20Preparation/JS-Back-End/Exam-From-1-November"
+                        image={tutorials}
+                        technologies={academy}
+                    />
+                    <Project
+                        name="Shoes shop - Front-end with templates"
+                        description="This is web site where you can have acount, by diferent shoes, create, modify and delete shoes offer that you create."
+                        href="https://github.com/TatyanaBeneva/SoftUni-Courses/tree/master/Final%20Exam%20Preparation/JS-Applications/Exam-Form-18-August-2020/ShoeShelf"
+                        image={shoe}
+                        technologies={shoes}
+                    />
+                    <Project
+                        name="Movies - Front-end with templates"
+                        description="This is web site where you can have acount, to create movies reviews, see other member reviews, modify and delete movies reviews that you create and like others."
+                        href="https://github.com/TatyanaBeneva/SoftUni-Courses/tree/master/Final%20Exam%20Preparation/JS-Applications/Exam-From-08-August-2020/Movies"
+                        image={movies}
+                        technologies={shoes}
+                    />
+                </UlLaptop>
+            }
+            {isTablet &&
+                <UlTablet>
+                    <Project
+                        name="Cours Academy - Back-end"
+                        description="This is web site where you can have acount, to enroll in a programming course of your choice, to create, modify and delete courses that you create."
+                        href="https://github.com/TatyanaBeneva/SoftUni-Courses/tree/master/Final%20Exam%20Preparation/JS-Back-End/Exam-From-1-November"
+                        image={tutorials}
+                        technologies={academy}
+                    />
+                    <Project
+                        name="Shoes shop - Front-end with templates"
+                        description="This is web site where you can have acount, by diferent shoes, create, modify and delete shoes offer that you create."
+                        href="https://github.com/TatyanaBeneva/SoftUni-Courses/tree/master/Final%20Exam%20Preparation/JS-Applications/Exam-Form-18-August-2020/ShoeShelf"
+                        image={shoe}
+                        technologies={shoes}
+                    />
+                    <Project
+                        name="Movies - Front-end with templates"
+                        description="This is web site where you can have acount, to create movies reviews, see other member reviews, modify and delete movies reviews that you create and like others."
+                        href="https://github.com/TatyanaBeneva/SoftUni-Courses/tree/master/Final%20Exam%20Preparation/JS-Applications/Exam-From-08-August-2020/Movies"
+                        image={movies}
+                        technologies={shoes}
+                    />
+                </UlTablet>
+            }
         </div>
     )
 }
 
-const Ul = styled.ul`
+const UlTablet = styled.ul`
+    padding: 0;
+    margin: 50px 20px;
+`
+
+const UlLaptop = styled.ul`
     padding: 0;
     margin: 50px 100px;
 `
